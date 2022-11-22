80ee173e25ed26b1c39710a46b093b88d4823c0d
David Zambrano <david._101@hotmail.com>
2022-11-22 18:24:12 -0500

    Display pokemons and implement categories

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 import './styles.css'
+import {  loopPokemons } from './modules/pokemons';
+import { loopFirePokemons } from './categories/fire';
+import { loopWaterPokemons } from './categories/water';
+
+const pokebtn = document.getElementById('poke');
+const catebtn = document.getElementById('catego');
+const home = document.querySelector('.home');
+const catego = document.querySelector('.categories');
+const activebtn = document.querySelectorAll('.activebtn');
+const parentDiv = document.querySelector('.pokemon-container');
+const electric = document.querySelectorAll('.electric');
+const normal = document.querySelectorAll('.normal');
+const fire = document.querySelectorAll('.fire');
+const water = document.querySelectorAll('.water');
+const ice = document.querySelectorAll('.ice');
+const rock = document.querySelectorAll('.rock');
+const flying = document.querySelectorAll('.flying');
+const grass = document.querySelectorAll('.grass');
+const ghost = document.querySelectorAll('.ghost');
+const bug = document.querySelectorAll('.bug');
+const poison = document.querySelectorAll('.poison');
+const ground = document.querySelectorAll('.ground');
+const fighting = document.querySelectorAll('.fighting');
+const dragon = document.querySelectorAll('.dragon');
+const steel= document.querySelectorAll('.steel');
+
+
+
+ 
+
+//Toogle pages --start--
+const addDisplayPoke = () => {
+ home.classList.replace('none', 'active');
+ catego.classList.replace('active', 'none');
+ }
+ 
+ const addDisplayBtn = () => {
+  home.classList.replace('active', 'none');
+ catego.classList.replace('none', 'active');
+ }
+
+pokebtn.addEventListener('click', addDisplayPoke);
+catebtn.addEventListener('click', addDisplayBtn);
+
+//Toogle pages --end--
+
+ const firebtn = document.getElementById('Fire');
+ firebtn.addEventListener('click', function(){
+  loopFirePokemons(200);
+})
+ const waterbtn = document.getElementById('Water');
+ waterbtn.addEventListener('click', function(){
+  loopWaterPokemons(200);
+})
+
+
+
+loopPokemons(180)
---
+++ src/modules/pokemons.js
@@ -0,0 +1,45 @@
+const fetchPokemons = async (id) => {
+ const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+ const data = await response.json();
+ renderPokemons(data);
+}
+
+export const loopPokemons = (number) => {
+ for (let i = 1; i<= number; i++){
+  fetchPokemons(i);
+ }
+}
+
+
+const renderPokemons = (pokemon) => {
+ const pokemonContainer = document.querySelector(".pokemon-container");
+ const card = document.createElement('div');
+ card.classList.add('pokemonBlock');
+ let  types  = pokemon.types[0].type.name;
+ 
+ const gifContainer = document.createElement('div');
+ gifContainer.classList.add('gifContainer')
+
+ const gif = document.createElement('img');
+ gif.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`
+ gif.classList.add(`${types}`)
+ gifContainer.appendChild(gif);
+
+ const number = document.createElement('p');
+ number.textContent = `#${pokemon.id}`;
+
+ const name = document.createElement('p');
+ name.classList.add('name');
+ name.textContent = pokemon.name;
+
+ const TYPE = document.createElement('p');
+ TYPE.classList.add('TYPE');
+ TYPE.textContent = types.toUpperCase();
+
+ card.appendChild(gifContainer);
+ card.appendChild(number);
+ card.appendChild(name);
+ card.appendChild(TYPE);
+
+ pokemonContainer.appendChild(card);
+}
