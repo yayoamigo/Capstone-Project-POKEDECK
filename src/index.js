8416e7cb66e55286738e4033127491b38d4b4318
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-22 20:38:24 -0300

    Add connection to api detail

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css'
+import Pokemon from './modules/Pokemon'
+import { render, spinner } from './modules/Functions'
+
+const closeButton = document.querySelector('.button-close');
+const modal = document.querySelector('.modal-background');
+const random = document.querySelector('#random');
+
+closeButton.addEventListener('click', () => {
+  modal.classList.add('display-none');
+});
+
+random.addEventListener('click', () => {
+  spinner(true)
+  const randomNumber = Math.ceil(Math.random() * 1153) + 1;
+  Pokemon.getPokemon(randomNumber).then(
+    result => render(result)
+  )
+
+})
---
+++ src/modules/Functions.js
@@ -0,0 +1,40 @@
+const titleNode = document.querySelector('.modal-title');
+const imageNode = document.querySelector('.modal-image');
+const modalDataList = document.querySelector('.modal-data-list');
+const modalStatsList = document.querySelector('.modal-stats-list');
+const imageEndpoint = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'
+const spinnerNode = document.querySelector('.wrapper');
+const modal = document.querySelector('.modal-background');
+//<li class="modal-data-item grass">grass</li>
+//<li class="modal-stats-item"><p class="modal-stat-name">hp</p><p class="modal-stat-number">45</p></li>
+const render = (data) => {
+  const {id, name, sprites, stats, types} = data
+  titleNode.textContent = name.toUpperCase();
+  imageNode.src = `${imageEndpoint}${id}.png`
+  modalStatsList.innerHTML='';
+  stats.forEach((stat)=>{
+    const list = document.createElement('li')
+    list.innerHTML=`<li class="modal-stats-item"><p class="modal-stat-name">${stat.stat.name}</p><p class="modal-stat-number">${stat.base_stat}</p></li>`
+    modalStatsList.appendChild(list);
+  });
+  modalDataList.innerHTML='';
+  types.forEach((type) => {
+    const list = document.createElement('li');
+    list.innerHTML=`<li class="modal-data-item ${type.type.name}">${type.type.name}</li>`;
+    modalDataList.appendChild(list);
+  });
+  spinner(false)
+  modal.classList.remove('display-none')
+  
+  console.log('DATA RECEIVED', data);
+}
+
+const spinner = (state) => {
+  if(state){
+    spinnerNode.classList.remove('display-none');
+  }else{
+    spinnerNode.classList.add('display-none');
+  }
+}
+
+export { render, spinner };
