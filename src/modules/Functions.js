842cfd3b314868a44ff6c04a14b1cb22c536a36a
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-24 19:21:46 -0300

    fix counter

--- src/modules/Functions.js
+++ src/modules/Functions.js
@@ -1,4 +1,5 @@
 import Comment from './Comments.js';
+import commentsCount from './CommentsCounter.js';
 
 const titleNode = document.querySelector('.modal-title');
 const imageNode = document.querySelector('.modal-image');
@@ -24,8 +25,14 @@ const render = (data) => {
     id, name, stats, types, result,
   } = data;
   const comments = result.error ? [] : result;
+  count.textContent = 0;
   itemIdNode.value = id;
-  count.textContent = comments.length;
+  commentsCount(id).then(
+    (result) => {
+      count.textContent = result;
+      return result;
+    },
+  );
   titleNode.textContent = name.toUpperCase();
   imageNode.src = `${imageEndpoint}${id}.png`;
   modalStatsList.innerHTML = '';
