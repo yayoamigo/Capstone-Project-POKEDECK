5379df053c912665e54716ca1771551886f514bc
David Zambrano <david._101@hotmail.com>
2022-11-23 21:55:25 -0500

    Update index js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import './styles.css';
 import loopPokemons from './modules/pokemons.js';
-import loopFirePokemons from './modules/categories/fire.js';
-import loopWaterPokemons from './modules/categories/water.js';
-import loopFlyingPokemons from './modules/categories/flying.js';
-import loopBugsPokemons from './modules/categories/bugs.js';
-import loopDragonPokemons from './modules/categories/dragon.js';
-import loopElectricPokemons from './modules/categories/electric.js';
-import loopGhostPokemons from './modules/categories/ghost.js';
-import loopGrassPokemons from './modules/categories/grass.js';
-import loopGroundPokemons from './modules/categories/ground.js';
-import loopIcePokemons from './modules/categories/ice.js';
-import loopNormalPokemons from './modules/categories/normal.js';
-import loopPoisonPokemons from './modules/categories/poison.js';
-import loopPsychicPokemons from './modules/categories/psychic.js';
-import loopRockPokemons from './modules/categories/rock.js';
 import Pokemon from './modules/Pokemon.js';
 import Comment from './modules/Comments.js';
 import { show, spinner } from './modules/Functions.js';
-
+ 
 const closeButton = document.querySelector('.button-close');
 const modal = document.querySelector('.modal-background');
 const random = document.querySelector('#random');
@@ -26,105 +12,43 @@ const pokebtn = document.getElementById('poke');
 const catebtn = document.getElementById('catego');
 const home = document.querySelector('.home');
 const catego = document.querySelector('.categories');
-
+const categoryBtn = document.querySelectorAll('.activebtn');
+ 
+ 
 // Toogle pages --start--
 const addDisplayPoke = () => {
   home.classList.replace('none', 'active');
   catego.classList.replace('active', 'none');
 };
-
+ 
 const addDisplayBtn = () => {
   home.classList.replace('active', 'none');
   catego.classList.replace('none', 'active');
 };
-
+ 
 pokebtn.addEventListener('click', addDisplayPoke);
 catebtn.addEventListener('click', addDisplayBtn);
-
-// Toogle pages --end--
-
-const fireBtn = document.getElementById('Fire');
-fireBtn.addEventListener('click', () => {
-  loopFirePokemons(300);
-});
-
-const waterBtn = document.getElementById('Water');
-waterBtn.addEventListener('click', () => {
-  loopWaterPokemons(300);
-});
-
-const poisonBtn = document.getElementById('Poison');
-poisonBtn.addEventListener('click', () => {
-  loopPoisonPokemons(300);
-});
-
-const groundBtn = document.getElementById('Ground');
-groundBtn.addEventListener('click', () => {
-  loopGroundPokemons(300);
-});
-
-const flyingdBtn = document.getElementById('Flying');
-flyingdBtn.addEventListener('click', () => {
-  loopFlyingPokemons(300);
-});
-
-const ghostBtn = document.getElementById('Ghost');
-ghostBtn.addEventListener('click', () => {
-  loopGhostPokemons(300);
-});
-
-const normalBtn = document.getElementById('Normal');
-normalBtn.addEventListener('click', () => {
-  loopNormalPokemons(200);
-});
-
-const bugBtn = document.getElementById('Bugs');
-bugBtn.addEventListener('click', () => {
-  loopBugsPokemons(300);
-});
-
-const psychicBtn = document.getElementById('Psychic');
-psychicBtn.addEventListener('click', () => {
-  loopPsychicPokemons(300);
-});
-
-const dragonBtn = document.getElementById('Dragon');
-dragonBtn.addEventListener('click', () => {
-  loopDragonPokemons(300);
-});
-
-const electricBtn = document.getElementById('Electric');
-electricBtn.addEventListener('click', () => {
-  loopElectricPokemons(300);
-});
-
-const grassBtn = document.getElementById('Grass');
-grassBtn.addEventListener('click', () => {
-  loopGrassPokemons(300);
-});
-
-const iceBtn = document.getElementById('Ice');
-iceBtn.addEventListener('click', () => {
-  loopIcePokemons(300);
-});
-
-const rockBtn = document.getElementById('Rock');
-rockBtn.addEventListener('click', () => {
-  loopRockPokemons(300);
-});
-
+ 
+// Toogle page --end--
+ 
 loopPokemons(200);
+categoryBtn.forEach((name) => {
+  name.addEventListener('click', () => {
+    loopPokemons(300, name.id.toLowerCase());
+  });
+});
+ 
 closeButton.addEventListener('click', () => {
   modal.classList.add('display-none');
 });
 
-random.addEventListener('click', () => {
-  spinner(true);
-  const randomNumber = Math.ceil(Math.random() * 1153) + 1;
-  Pokemon.getPokemon(randomNumber).then(
-    (result) => show(result),
-  );
-});
+// random.addEventListener('click', () => {
+//   spinner(true);
+//   const randomNumber = Math.ceil(Math.random() * 1153) + 1;
+//   Pokemon.getPokemon(randomNumber).then(
+//     (result) => show(result),
+//   );
+// });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
--- src/modules/pokemons.js
+++ src/modules/pokemons.js
@@ -1,46 +1,62 @@
-const renderPokemons = (pokemon) => {
-  const pokemonContainer = document.querySelector('.pokemon-container');
-  const card = document.createElement('div');
-  card.classList.add('pokemonBlock');
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+ 
+const renderPokemons = (pokemon, category) => {
   const types = pokemon.types[0].type.name;
-
-  const gifContainer = document.createElement('div');
-  gifContainer.classList.add('gifContainer');
-
-  const gif = document.createElement('img');
-  gif.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`;
-  gif.classList.add(`${types}`);
-  gifContainer.appendChild(gif);
-
-  const number = document.createElement('p');
-  number.textContent = `#${pokemon.id}`;
-
-  const name = document.createElement('p');
-  name.classList.add('name');
-  name.textContent = pokemon.name;
-
-  const TYPE = document.createElement('p');
-  TYPE.classList.add('TYPE');
-  TYPE.textContent = types.toUpperCase();
-
-  card.appendChild(gifContainer);
-  card.appendChild(number);
-  card.appendChild(name);
-  card.appendChild(TYPE);
-
-  pokemonContainer.appendChild(card);
-};
-
-const fetchPokemons = async (id) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-  const data = await response.json();
-  renderPokemons(data);
-};
-
-const loopPokemons = (number) => {
-  for (let i = 1; i <= number; i += 1) {
-    fetchPokemons(i);
+  let pokemonContainer;
+  if (category === undefined) {
+    pokemonContainer = document.querySelector('.pokemon-container');
+  } else {
+    pokemonContainer = document.querySelector('.pokemon-container-group');
+  }
+ 
+  if (category === undefined || category === types) {
+    const card = document.createElement('div');
+    card.classList.add('pokemonBlock');
+ 
+    const gifContainer = document.createElement('div');
+    gifContainer.classList.add('gifContainer');
+ 
+    const gif = document.createElement('img');
+    gif.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`;
+    gif.classList.add(`${types}`);
+    gifContainer.appendChild(gif);
+ 
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id}`;
+ 
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+ 
+    const TYPE = document.createElement('p');
+    TYPE.classList.add('TYPE');
+    TYPE.textContent = types.toUpperCase();
+ 
+    card.appendChild(gifContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+    card.appendChild(TYPE);
+ 
+    pokemonContainer.appendChild(card);
   }
 };
-
+//Fetch function
+const fetchPokemons = async (id, category) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  const data = await response.json();
+  renderPokemons(data, category);
+};
+ 
+const loopPokemons = (number, category) => {
+  const pokemonContainerGroup = document.querySelector('.pokemon-container-group');
+  removeChildNodes(pokemonContainerGroup);
+  for (let i = 1; i <= number; i += 1) {
+    fetchPokemons(i, category);
+  }
+};
+ 
 export default loopPokemons;
