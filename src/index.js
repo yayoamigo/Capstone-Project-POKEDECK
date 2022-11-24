8a29330dff8f4824811263902b9a0c5dbaf9cad3
David Zambrano <david._101@hotmail.com>
2022-11-23 21:10:57 -0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import './styles.css';
 import loopPokemons from './modules/pokemons.js';
-<<<<<<< HEAD
-=======
-import loopFirePokemons from './modules/categories/fire.js';
-import loopWaterPokemons from './modules/categories/water.js';
-import loopFlyingPokemons from './modules/categories/flying.js';
-import loopBugsPokemons from './modules/categories/bugs.js';
-import loopDragonPokemons from './modules/categories/dragon.js';
-import loopElectricPokemons from './modules/categories/electric.js';
-import loopGhostPokemons from './modules/categories/ghost.js';
-import loopGrassPokemons from './modules/categories/grass.js';
-import loopGroundPokemons from './modules/categories/ground.js';
-import loopIcePokemons from './modules/categories/ice.js';
-import loopNormalPokemons from './modules/categories/normal.js';
-import loopPoisonPokemons from './modules/categories/poison.js';
-import loopPsychicPokemons from './modules/categories/psychic.js';
-import loopRockPokemons from './modules/categories/rock.js';
->>>>>>> 9b5016b0fb3956bb47c8bd9dd890580a326958bc
 import Pokemon from './modules/Pokemon.js';
 import Comment from './modules/Comments.js';
 import { show, spinner } from './modules/Functions.js';
@@ -30,10 +13,7 @@ const catebtn = document.getElementById('catego');
 const home = document.querySelector('.home');
 const catego = document.querySelector('.categories');
 const categoryBtn = document.querySelectorAll('.activebtn');
-const closeButton = document.querySelector('.button-close');
-const modal = document.querySelector('.modal-background');
-const random = document.querySelector('#random');
-const form = document.querySelector('.modal-form-comment');
+
 
 // Toogle pages --start--
 const addDisplayPoke = () => {
@@ -52,15 +32,12 @@ catebtn.addEventListener('click', addDisplayBtn);
 // Toogle pages --end--
 
 loopPokemons(200);
-<<<<<<< HEAD
 categoryBtn.forEach((name) => {
   name.addEventListener('click', () => {
     loopPokemons(300, name.id.toLowerCase());
   });
 });
 
-=======
->>>>>>> 9b5016b0fb3956bb47c8bd9dd890580a326958bc
 closeButton.addEventListener('click', () => {
   modal.classList.add('display-none');
 });
@@ -96,8 +73,4 @@ form.addEventListener('submit', (e) => {
         throw new Error('Error creating post');
       }
     });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 9b5016b0fb3956bb47c8bd9dd890580a326958bc
